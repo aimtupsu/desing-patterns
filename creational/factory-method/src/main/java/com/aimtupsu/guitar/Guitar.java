70e997e19a2b9686dfe0ddd81c09cd86f5e93d06
Vladimir.Shchepin <vladimir.shchepin@x5.ru>
2020-09-12 16:35:06 +0300

    add Factory Method design pattern
    Also:
     - add maven module for creationals patterns;
     - add logger - log4j2.

---
+++ creational/factory-method/src/main/java/com/aimtupsu/guitar/Guitar.java
@@ -0,0 +1,14 @@
+package com.aimtupsu.guitar;
+
+/**
+ * Интерфейс всех гитар.
+ *
+ * @author Vladimir.Shchepin
+ */
+public interface Guitar {
+
+    /**
+     * Гитара умеет играть.
+     */
+    void play();
+}
---
+++ creational/factory-method/src/main/java/com/aimtupsu/workshop/impl/BassGuitarWorkshop.java
@@ -0,0 +1,32 @@
+package com.aimtupsu.workshop.impl;
+
+import com.aimtupsu.guitar.Guitar;
+import com.aimtupsu.guitar.impl.BassGuitar;
+import com.aimtupsu.workshop.GuitarWorkshop;
+import javax.annotation.Nonnull;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Мастерская, в которой создают басс гитары.
+ *
+ * @author Vladimir.Shchepin
+ */
+@Log4j2
+public class BassGuitarWorkshop implements GuitarWorkshop {
+
+    /**
+     * Количество струн у басс гитары.
+     */
+    private static final int STRING_COUNT = 4;
+
+    public BassGuitarWorkshop() {
+        log.info("Создание мастерской басс гитар.");
+    }
+
+    @Override
+    @Nonnull
+    public Guitar createGuitar() {
+        log.info("Создание новой басс гитары.");
+        return new BassGuitar(STRING_COUNT);
+    }
+}
